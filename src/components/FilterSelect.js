1f6893ba807ba3bdf5f0ef2d30747426aed989d2
Mansoor Roeen <drunkenpirate47x@gmail.com>
2022-02-14 23:45:52 +0530

    changes made

---
+++ src/components/FilterSelect.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function FilterSelect() {
+  
+  return (
+
+    <MainWrapper>  
+      
+    </MainWrapper>
+
+  )
+}
+
+
+
+const MainWrapper = styled.div `
+  display: flex;
+  height: auto; 
+  justify-content: space-between;
+  align-items: center;
+
+  width: 100%;
+`;
